dd8cd105e376ae58bf20c33e6f700b91b1d12628
NasoloEricka <anjaranasoloericka@gmail.com>
2023-01-31 09:56:07 +0300

    Update EnchereController.java

--- src/main/java/enchere/enchere/controller/EnchereController.java
+++ src/main/java/enchere/enchere/controller/EnchereController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ public class EnchereController {
         return map;
     }
 
-    @GetMapping("/search")
+    @PostMapping("/search")
     public Map<String, Object> recherche(@RequestBody RechercheEnchere recherche) {
         Enchere e = new Enchere();
         String condition = (e.getSearchSQL(recherche));
